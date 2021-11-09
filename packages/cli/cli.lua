4ba8254e986b9cf6ad7ae1d86bb4f6b76a7a8f46
Luke Bennett <LukeeeeBennettPlus@gmail.com>
2021-11-09 21:22:18 +0000

    feat(cli): add cli package

---
+++ packages/cli/cli.lua
@@ -0,0 +1,18 @@
+os.loadAPI('/blink/strings.lua')
+
+function parseFlags(args)
+  local commands = {}
+  local flags = {}
+
+  for _, a in ipairs(args) do
+    local flag, value = a:match('^--(.+)=(.+)')
+
+    if flag then
+      flags[flag] = value
+    else
+      table.insert(commands, a)
+    end
+  end
+
+  return commands, flags
+end
